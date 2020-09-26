349b9caa4bda8c9b2d0054ebcf942d38221ad851
Hyungu Kang <hyunguboy@gmail.com>
2020-09-26 15:40:42 +0900

    Add files via upload

---
+++ HKang_InnovationEquation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib
+
+# 2020 Hyungu Kang, www.hazykinetics.com, hyunguboy@gmail.com
+# Replicate compensation model from "Loonshots" by Safi Bahcall
+
+# Compensation amounts. Numbers here are an arbitrary base number, not a specific currency.
+CompBase = 100
+CompEquity = 10
+CompTotal = CompBase + CompEquity
+
+print(CompTot)
